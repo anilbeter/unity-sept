df9f1d17e9051cf05b76586fe84d6b2f425752d6
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-08-30 14:45:41 +0300

    implement if else statement for directionX

--- PhysMiniGame/Assets/Scripts/GatherInput.cs
+++ PhysMiniGame/Assets/Scripts/GatherInput.cs
@@ -6,10 +6,16 @@ using UnityEngine.InputSystem;
 public class GatherInput : MonoBehaviour
 {
     // Since this is a simple project, will be use simple input solution
-
+    public int directionX = 0;
 
     void Update()
     {
+        if (Keyboard.current.dKey.isPressed)
+            directionX = 1;
+        else if (Keyboard.current.aKey.isPressed)
+            directionX = -1;
+        else
+            directionX = 0;
 
     }
 }
