2ed5fc8d863cf4ca8db54b2b7d2d355c3630ad9f
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-08-30 14:48:05 +0300

    add speed and rigidbody variables

--- PhysMiniGame/Assets/Scripts/PlayerMove.cs
+++ PhysMiniGame/Assets/Scripts/PlayerMove.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    public float speed;
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
