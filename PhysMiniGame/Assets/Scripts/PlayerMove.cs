7efc1bb8e4e7a7dfc32bf1b4293818953017e950
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-08-30 14:55:14 +0300

    implement player movement functionality

--- PhysMiniGame/Assets/Scripts/PlayerMove.cs
+++ PhysMiniGame/Assets/Scripts/PlayerMove.cs
@@ -6,15 +6,17 @@ public class PlayerMove : MonoBehaviour
 {
     public float speed;
     private Rigidbody2D rb;
+    private GatherInput gatherInput;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        gatherInput = GetComponent<GatherInput>();
     }
 
     private void FixedUpdate()
     {
-
+        rb.velocity = new Vector2(speed * gatherInput.directionX, 0);
     }
 }
