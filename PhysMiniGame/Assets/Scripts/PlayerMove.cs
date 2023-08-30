b88355b4eec563fdbe89b8d9e41f130bc58ee0a1
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-08-30 15:03:07 +0300

    add onCollisionEnter2D method

--- PhysMiniGame/Assets/Scripts/PlayerMove.cs
+++ PhysMiniGame/Assets/Scripts/PlayerMove.cs
@@ -19,4 +19,9 @@ public class PlayerMove : MonoBehaviour
     {
         rb.velocity = new Vector2(speed * gatherInput.directionX, 0);
     }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        speed = 0;
+    }
 }
