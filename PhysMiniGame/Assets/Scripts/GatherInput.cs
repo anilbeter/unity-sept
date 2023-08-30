b48567b640338e2ded64bc27924f80e653df5dd6
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-08-30 14:42:25 +0300

    create GatherInput script

---
+++ PhysMiniGame/Assets/Scripts/GatherInput.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GatherInput : MonoBehaviour
+{
+    // Since this is a simple project, will be use simple input solution
+
+
+    void Update()
+    {
+
+    }
+}
