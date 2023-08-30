49978164224f50b9fb2aa5ec95c5f8a826f9be87
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-08-30 15:34:10 +0300

    create random instantiates

--- PhysMiniGame/Assets/Scripts/Spawner.cs
+++ PhysMiniGame/Assets/Scripts/Spawner.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    public GameObject enemyCircle;
+    public Transform[] spawnPoint;
+    private float timer;
+    public float spawnDelay;
 
     void Start()
     {
-
+        // Instantiate(enemyCircle, transform.position, transform.rotation);
+        timer = Time.time + spawnDelay;
     }
 
 
     void Update()
     {
-
+        if (timer < Time.time)
+        {
+            // spawn
+            int i = Random.Range(0, spawnPoint.Length);
+            Instantiate(enemyCircle, spawnPoint[i].position, transform.rotation);
+            timer += spawnDelay;
+        }
     }
 }
