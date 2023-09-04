48775454c4372851360861c2f6d24a7eee0bfbd7
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-04 09:29:01 +0300

    replace MonoBehavioru with Enemy

--- 2D Platformer/Assets/Scripts/PatrollingEnemy.cs
+++ 2D Platformer/Assets/Scripts/PatrollingEnemy.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PatrollingEnemy : MonoBehaviour
+// MonoBehaviour yerine Enemy yazdım. Şu an Enemy parent class, PatrollingEnemy child class oldu.
+public class PatrollingEnemy : Enemy
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
