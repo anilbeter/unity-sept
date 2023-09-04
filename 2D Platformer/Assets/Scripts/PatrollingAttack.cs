d3e32b09c0f46acddac9c4267dc2a42f05e7e50c
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-04 10:14:02 +0300

    create PatrollingAttack and EnemyAttack scripts

---
+++ 2D Platformer/Assets/Scripts/PatrollingAttack.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// EnemyAttack yazdım MonoBehaviour yerine, şimdi EnemyAttack'in childı oldu
+public class PatrollingAttack : EnemyAttack
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
