dfd0c7071a3be774d717a69d5494f23ee9ef6517
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-04 10:09:00 +0300

    setup enemy patrolling script and draw circles for detect wall & ground

--- 2D Platformer/Assets/Scripts/PatrollingEnemy.cs
+++ 2D Platformer/Assets/Scripts/PatrollingEnemy.cs
@@ -1,11 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 // MonoBehaviour yerine Enemy yazdım. Şu an Enemy parent class, PatrollingEnemy child class oldu.
 public class PatrollingEnemy : Enemy
 {
-    // Start is called before the first frame update
+    public float speed;
+    private int direction = -1;
+
+    public Transform groundcheck;
+    public Transform wallCheck;
+    public LayerMask layerToCheck;
+
+    private bool detectGround;
+    private bool detectWall;
+
+    public float radius;
+
     void Start()
     {
 
@@ -13,6 +25,21 @@ public class PatrollingEnemy : Enemy
 
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(-1, rb.velocity.y);
+        Flip();
+        rb.velocity = new Vector2(direction * speed, rb.velocity.y);
+    }
+
+    private void Flip()
+    {
+        // Draw circle for detection ground
+        detectGround = Physics2D.OverlapCircle(groundcheck.position, radius, layerToCheck);
+        // Draw circle for detection wall
+        detectWall = Physics2D.OverlapCircle(wallCheck.position, radius, layerToCheck);
+
+        if (detectWall || !detectGround)
+        {
+            direction *= -1;
+            transform.localScale = new Vector3(-transform.localScale.x, 1, 1);
+        }
     }
 }
