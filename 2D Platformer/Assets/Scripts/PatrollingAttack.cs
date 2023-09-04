50f15b9c85f91cc3994072f33825412a6ad324b7
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-04 10:32:03 +0300

    add knocback to PatrollingAttacks

--- 2D Platformer/Assets/Scripts/PatrollingAttack.cs
+++ 2D Platformer/Assets/Scripts/PatrollingAttack.cs
@@ -5,5 +5,17 @@ using UnityEngine;
 // EnemyAttack yazdım MonoBehaviour yerine, şimdi EnemyAttack'in childı oldu
 public class PatrollingAttack : EnemyAttack
 {
+    PlayerMoveControls playerMove;
+    public float forceX;
+    public float forceY;
+    public float duration;
 
+    public override void SpecialAttack()
+    {
+        // bizim virtual fonksiyonun içi boş, ama dolu olsaydı yani içinde kod olsaydı base.SpecialAttack() kodu sayesinde oradaki bütün kodları çağırabilcektim
+        base.SpecialAttack();
+
+        playerMove = playerStats.GetComponentInParent<PlayerMoveControls>();
+        StartCoroutine(playerMove.KnockBack(forceX, forceY, duration, transform.parent));
+    }
 }
