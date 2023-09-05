5123b48d3db13fd6511bf25750e2a6d72fb26738
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-05 10:35:41 +0300

    create Fader script

---
+++ 2D Platformer/Assets/Scripts/Fader.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class Fader : MonoBehaviour
+{
+    private Animator anim;
+    private int lvlToLoad;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void SetLvl(int lvl)
+    {
+        lvlToLoad = lvl;
+        anim.SetTrigger("Fade");
+    }
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(lvlToLoad);
+    }
+}
