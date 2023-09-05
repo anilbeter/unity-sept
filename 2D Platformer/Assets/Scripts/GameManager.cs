8f553064a5be4e40f882915c484b28b8f673cd2c
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-05 10:59:17 +0300

    add awake function with DontDestoryOnLoad method

--- 2D Platformer/Assets/Scripts/GameManager.cs
+++ 2D Platformer/Assets/Scripts/GameManager.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+
     }
+
 }
