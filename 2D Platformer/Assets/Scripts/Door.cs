706f2a2b2be35cf28a7ac4f747f2c3d31b0c7b5b
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-05 10:18:43 +0300

    add LoadLevel function and implement switching levels

--- 2D Platformer/Assets/Scripts/Door.cs
+++ 2D Platformer/Assets/Scripts/Door.cs
@@ -2,17 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.SceneManagement;
+
 public class Door : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int lvlToLoad;
+
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LoadLevel()
     {
-        
+        SceneManager.LoadScene(lvlToLoad);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            GetComponent<BoxCollider2D>().enabled = false;
+            collision.GetComponent<GatherInput>().DisableControls();
+
+            LoadLevel();
+        }
     }
 }
