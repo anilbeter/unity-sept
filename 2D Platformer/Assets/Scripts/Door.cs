8f0161e564bf4621ee43e1e0a3cecc4421e37d27
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-05 10:23:18 +0300

    delete some codes in door script (starting fade effect while switching lvl)

--- 2D Platformer/Assets/Scripts/Door.cs
+++ 2D Platformer/Assets/Scripts/Door.cs
@@ -2,27 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour
 {
     public int lvlToLoad;
 
-
-    void Start()
-    {
-
-    }
-
-    private void LoadLevel()
-    {
-        SceneManager.LoadScene(lvlToLoad);
-    }
-
-    private void RestartLevel()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -30,8 +14,6 @@ public class Door : MonoBehaviour
         {
             GetComponent<BoxCollider2D>().enabled = false;
             collision.GetComponent<GatherInput>().DisableControls();
-
-            LoadLevel();
         }
     }
 }
