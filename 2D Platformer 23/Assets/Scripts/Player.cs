0b7889e0291e840a1d0df31c64a8076d424b40dc
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-08 10:32:48 +0300

    add first script

---
+++ 2D Platformer 23/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float moveSpeed;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+    }
+}
