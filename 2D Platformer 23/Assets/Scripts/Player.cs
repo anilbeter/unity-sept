de8d0242a5896f00afce67c3e3325c1dadd0574d
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-09 08:42:06 +0300

    add jump and fall animations in code

--- 2D Platformer 23/Assets/Scripts/Player.cs
+++ 2D Platformer 23/Assets/Scripts/Player.cs
@@ -45,6 +45,9 @@ public class Player : MonoBehaviour
     {
         bool isMoving = rb.velocity.x != 0;
         anim.SetBool("isMoving", isMoving);
+
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetFloat("yVelocity", rb.velocity.y);
     }
 
     private void InputChecks()
